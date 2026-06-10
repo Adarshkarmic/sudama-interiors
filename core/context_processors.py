6cf2bfecf0b2499d235d2490667a1f624aa36e4d
adarsh <adarshkarmic111@gmail.com>
2026-06-10 16:22:15 +0530

    Update contact email and address

--- core/context_processors.py
+++ core/context_processors.py
@@ -5,6 +5,6 @@ def site_navigation(request):
     return {
         "nav_services": Service.objects.order_by("id"),
         "brand_phone": "+91 98765 43210",
-        "brand_email": "studio@sudamainteriors.com",
-        "brand_location": "Premium interior services in Darbhanga, Bihar",
+        "brand_email": "enterprisessudama734@gmail.com",
+        "brand_location": "MRM college road lalbagh ( Darbhanga )",
     }
