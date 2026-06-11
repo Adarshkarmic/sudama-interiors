72066a94e1b45e7c50d1a3f0320b4602bfc68123
adarsh <adarshkarmic111@gmail.com>
2026-06-11 22:50:51 +0530

    Update contact phone number

--- core/context_processors.py
+++ core/context_processors.py
@@ -4,7 +4,7 @@ from services.models import Service
 def site_navigation(request):
     return {
         "nav_services": Service.objects.order_by("id"),
-        "brand_phone": "+91 98765 43210",
+        "brand_phone": "+91 87574 77519",
         "brand_email": "enterprisessudama734@gmail.com",
         "brand_location": "MRM college road lalbagh (Darbhanga)",
     }
